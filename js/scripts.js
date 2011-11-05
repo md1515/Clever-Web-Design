60bb1ae608b4f061e8662e5b6d6b728ea0b0c95d
Bill Patrianakos <bill@chooseclever.com>
2011-11-05 15:47:14 -0500

    Have the homepage partially finished - about half of it - and we need to create the client management app and add analytics

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 /*
  * Put all your JavaScript scripts here. Plugins go elsewhere.
  */
+ 
+// Slider Init
+
+$(function(){
+			$('#slides').slides({
+				preload: true,
+				generateNextPrev: true,
+				play: 5500,
+				generatePagination: false,
+				next: 'next',
+				prev: 'prev'
+			});
+		});
