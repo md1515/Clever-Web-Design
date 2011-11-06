9fc184cb4a336b30ff2d490a595ed5127bc49077
Bill Patrianakos <bill@chooseclever.com>
2011-11-06 00:10:39 -0500

    Added most of the js and separated as much logic per page as possible. Still on the homepage

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,49 @@ $(function(){
 				prev: 'prev'
 			});
 		});
+		
+// FANCYBOX
+
+$(document).ready(function() {
+
+	
+	$("a.fancyinit").fancybox({
+		'hideOnContentClick': false
+	});
+
+	/* Apply fancybox to multiple items */
+	
+	$("a.group").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	true
+	});
+	
+});
+
+// FADE IN - FADE OUT
+
+$(function() {
+// OPACITY OF BUTTON SET TO 50%
+$(".fadeinout").css("opacity","0.5");
+
+// ON MOUSE OVER
+$(".fadeinout").hover(function () {
+
+// SET OPACITY TO 100%
+$(this).stop().animate({
+opacity: 1.0
+}, "slow");
+},
+
+// ON MOUSE OUT
+function () {
+
+// SET OPACITY BACK TO 50%
+$(this).stop().animate({
+opacity: 0.5
+}, "slow");
+});
+});
