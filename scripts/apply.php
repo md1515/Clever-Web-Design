0ed443042c7077654dd1fcab880d9d8248582c89
Bill Patrianakos <bill@chooseclever.com>
2011-11-14 00:29:30 -0600

    Secured the mail script as much as I could. Needs testing and more research

--- scripts/apply.php
+++ scripts/apply.php
@@ -1,20 +1,63 @@
 <?php
 
-// Get the POST variables
-$name = $_POST[name];
-$applicant_email = $_POST[email];
-$phone = $_POST[phone];
-$position = $_POST[position];
-$age = $_POST[birthday];
-$employer = $_POST[employer];
-$experience = $_POST[experience];
-$why = $_POST[why];
-$personality = $_POST[personality];
+/***********************************\
+ * Somewhat secure PHP mail script *
+ * Version 0.5					   *
+ * Written by Bill Patrianakos	   *
+ * http://billpatrianakos.me 	   *
+\***********************************/
 
-// Send mail to:
-$mailto = "jobs@chooseclever.com";
+// Check for valid form submission
+if(isset($_POST['submit'])) {
+	// Check for required fields
+	$required = array('name', 'email', 'phone', 'birthday');
+		foreach($required as $field) {
+			if(!isset($_POST[$field]) || empty($_POST[$field]) ) {
+				$error[] = $field;
+				echo "You must at least include your name, email, phone number, and date of bith. Please <a href=\"../jobs/application.html\">Go back</a> and fill those in.";
+			}
+			else {
+				
+				// Sanitize and validate
+				// Set the POST info as variables
+				$applicantName = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+				$applicantEmail = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+					$applicantEmail = filter_var($applicantEmail, FILTER_VALIDATE_EMAIL);
+				$applicantPhone = filter_var($_POST['phone'], FILTER_SANITIZE_STRING);
+				$applicantPosition = filter_var($_POST['position'], FILTER_SANITIZE_STRING);
+				$applicantAge = filter_var($_POST['birthday'], FILTER_SANITIZE_STRING);
+				$applicantEmployer = filter_var($_POST['employer'], FILTER_SANITIZE_STRING);
+				$applicantExperience = filter_var($_POST['experience'], FILTER_SANITIZE_STRING);
+				$applicantWhy = filter_var($_POST['why'], FILTER_SANITIZE_STRING);
+				$applicantPersonality = filter_var($_POST['personality'], FILTER_SANITIZE_STRING);
+			}
+		}
 
-$message = "Name: $name \n Age: $age \n Phone number: $phone \n Applying for: $position \n Current employer: $emplyer \n Experience: $experience \n Why us? $why \n Personality: \n $personality";
+	// Do not let mail be sent unless via _POST request
+	if($_SERVER['REQUEST_METHOD'] != "POST") {
+	   echo("Error. Spam activity detected. Please try elsewhere.");
+	   exit;
+	}
+	else {
+		$email_to = "jobs@chooseclever.com";
+		$subject = "Clever Job Application";
+		$email_message = 'Name: ' . $applicantName . "\n";
+		$email_message .= "Age: " . $applicantAge  ."\n";
+		$email_message .= "Phone number: " . $applicantPhone . "\n";
+		$email_message .= "Applying for: " . $applicantPosition . "\n";
+		$email_message .= "Current employer: " . $applicantEmplyer . "\n";
+		$email_message .= "Experience: \n" . $applicantExperience . "\n";
+		$email_message .= "Why us? \n" . $applicantWhy . "\n";
+		$email_message .= "Personality: \n" $applicantPersonality . "\n";
 
+		// Send the mail
+		mail($email_to, $subject, $email_message);
+	}
+}
+// If submit was not set that indicates foul play. Throw an error and die.
+else {
+	echo "You are attempting to send an unauthorized message. Don't waste your time.";
+	exit;
+}
 
 ?>
